41bf69b2a6241271643706c41e5572738c728046
Sebastiano Barrera <sebastiano.barrera@gmail.com>
2025-05-06 00:16:02 +0200

    decompiler_tester: checkpoint

--- decompiler_tester/src/main.rs
+++ decompiler_tester/src/main.rs
@@ -1,5 +1,6 @@
 use std::{
     borrow::Cow,
+    fmt::Write,
     fs::File,
     path::{Path, PathBuf},
     time::Duration,
@@ -7,18 +8,20 @@ use std::{
 
 use anyhow::{Context, Result};
 use decompiler::test_tool::Tester;
+use egui::TextBuffer;
+use ouroboros::self_referencing;
 
-fn main() -> eframe::Result<()> {
+fn main() {
     let mut args = std::env::args();
     let self_name = args.next().unwrap();
     let Some(exe_filename) = args.next() else {
         eprintln!("Usage: {} EXE_FILENAME", self_name);
-        std::process::exit(1);
+        std::process::exit(1)
     };
 
     let exe_filename: PathBuf = exe_filename.into();
 
-    eframe::run_native(
+    let res = eframe::run_native(
         "decompiler test app",
         eframe::NativeOptions::default(),
         Box::new(|_cctx| {
@@ -26,17 +29,34 @@ fn main() -> eframe::Result<()> {
             app.open_executable(exe_filename);
             Ok(app)
         }),
-    )
+    );
+
+    if let Err(err) = res {
+        eprintln!("eframe error: {:?}", err);
+        std::process::exit(1);
+    }
+}
+
+#[self_referencing]
+struct Exe {
+    exe_bytes: Vec<u8>,
+
+    #[borrows(exe_bytes)]
+    #[covariant]
+    tester: Tester<'this>,
 }
 
 struct App {
-    state: State,
+    restore_file: RestoreFile,
+    exe: Option<Result<Exe>>,
+    ui_function_name: String,
+    process_log: String,
     file_view: FileView,
     status: StatusView,
 }
 
-#[derive(serde::Serialize, Default)]
-struct State {
+#[derive(serde::Serialize, Default, Clone)]
+struct RestoreFile {
     exe_filename: Option<PathBuf>,
     function_name: Option<String>,
 }
@@ -44,46 +64,103 @@ struct State {
 impl App {
     fn new() -> Self {
         App {
-            state: State::default(),
-            status: StatusView::default(),
+            restore_file: RestoreFile::default(),
+            exe: None,
+            ui_function_name: String::new(),
+            process_log: String::new(),
             file_view: FileView::default(),
+            status: StatusView::default(),
         }
     }
 
     fn open_executable(&mut self, path: PathBuf) {
-        // move off thread?
-
-        self.state = State {
+        self.restore(RestoreFile {
             exe_filename: Some(path),
             function_name: None,
-        };
+        });
+    }
 
-        let raw_binary = load_executable(path);
+    fn restore(&mut self, restore_file: RestoreFile) {
+        // move off thread?
+
+        let prev_restore_file = std::mem::replace(&mut self.restore_file, restore_file);
+
+        if self.restore_file.exe_filename != prev_restore_file.exe_filename {
+            self.exe = self
+                .restore_file
+                .exe_filename
+                .as_ref()
+                .map(|p| load_executable(&p));
+        }
+
+        if let Some(Ok(exe)) = &mut self.exe {
+            if self.restore_file.function_name != prev_restore_file.function_name {
+                match &self.restore_file.function_name {
+                    Some(function_name) => {
+                        exe.with_tester_mut(|tester| {
+                            let mut log = Vec::new();
+                            let mut pp = decompiler::pp::PrettyPrinter::start(&mut log);
+                            let res = tester.process_function(function_name, &mut pp);
+
+                            let mut log = String::from_utf8(log).unwrap();
+                            if let Err(err) = res {
+                                writeln!(log, "\nfinished with error: {:?}", err).unwrap();
+                            }
+
+                            self.process_log = log;
+                            self.ui_function_name = function_name.clone();
+                        });
+                    }
+                    None => {
+                        self.process_log = format!("No function loaded.");
+                    }
+                }
+            }
+        }
     }
 }
 
-fn load_executable(path: &Path) -> Result<Tester> {
+fn load_executable(path: &Path) -> Result<Exe> {
     use std::io::Read as _;
 
-    let mut raw_binary = Vec::new();
+    let mut exe_bytes = Vec::new();
     let mut elf = File::open(&path).context("opening file")?;
-    elf.read_to_end(&mut raw_binary).context("reading file")?;
+    elf.read_to_end(&mut exe_bytes).context("reading file")?;
 
-    Ok(decompiler::test_tool::Tester::start(&raw_binary))
+    ExeTryBuilder {
+        exe_bytes,
+        tester_builder: |exe_bytes| Tester::start(&exe_bytes).context("parsing executable"),
+    }
+    .try_build()
 }
 
 impl eframe::App for App {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         egui::CentralPanel::default().show(ctx, |ui| {
-            ui.label("Hello, world!");
-            if ui.button("Quit").clicked() {
-                ctx.send_viewport_cmd(egui::ViewportCommand::Close);
+            ui.horizontal(|ui| {
+                if ui.button("Quit").clicked() {
+                    ctx.send_viewport_cmd(egui::ViewportCommand::Close);
+                }
+                self.status.show(ui);
+            });
+
+            if ui
+                .text_edit_singleline(&mut self.ui_function_name)
+                .lost_focus()
+            {
+                self.restore(RestoreFile {
+                    function_name: Some(self.ui_function_name.clone()),
+                    ..self.restore_file.clone()
+                });
+                return;
             }
+
+            self.file_view.show(ui, &self.process_log);
         });
     }
 
     fn save(&mut self, storage: &mut dyn eframe::Storage) {
-        match ron::to_string(&self.state) {
+        match ron::to_string(&self.restore_file) {
             Ok(payload) => {
                 storage.set_string("state", payload);
                 self.status.push(StatusMessage {
@@ -103,26 +180,38 @@ impl eframe::App for App {
 }
 
 struct StatusView {
-    queue: Vec<StatusMessage>,
+    cur_msg: Option<StatusMessage>,
 }
 
 impl StatusView {
     fn push(&mut self, msg: StatusMessage) {
-        self.queue.push(msg);
+        eprintln!("status: {:?}", msg);
+        self.cur_msg = Some(msg);
+    }
+
+    fn show(&mut self, ui: &mut egui::Ui) {
+        if let Some(msg) = &self.cur_msg {
+            ui.horizontal(|ui| {
+                ui.label("Status:");
+                ui.label(msg.text.as_str());
+            });
+        }
     }
 }
 
 impl Default for StatusView {
     fn default() -> Self {
-        StatusView { queue: Vec::new() }
+        StatusView { cur_msg: None }
     }
 }
 
+#[derive(Debug)]
 struct StatusMessage {
     text: Cow<'static, str>,
     category: StatusCategory,
     timeout: Duration,
 }
+#[derive(Debug)]
 enum StatusCategory {
     Info,
     Error,
@@ -146,7 +235,7 @@ impl Default for FileView {
 }
 
 impl FileView {
-    fn show(&mut self, ctx: egui::Context) {
-        // --
+    fn show(&mut self, ui: &mut egui::Ui, log: &str) {
+        ui.label(log);
     }
 }
