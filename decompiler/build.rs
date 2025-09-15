bd5c0e09b3d4ae65254efa03db38b4f30976b0e1
Sebastiano Barrera <sebastiano.barrera@gmail.com>
2025-09-15 18:47:09 +0200

    gen_callconv_tests: integrate into build system

---
+++ decompiler/build.rs
@@ -0,0 +1,27 @@
+use std::process::Command;
+
+fn main() {
+    const SCRIPT_GEN_CALLCONV_TESTS: &str = "scripts/gen_callconv_tests.rb";
+
+    println!("cargo::rerun-if-changed={}", SCRIPT_GEN_CALLCONV_TESTS);
+    Command::new(SCRIPT_GEN_CALLCONV_TESTS)
+        .arg("--out-c")
+        .arg("test-data/x86_64_callconv.c")
+        .arg("--out-rs")
+        .arg("tests/generated_callconv.rs")
+        .status()
+        .unwrap();
+
+    // generate the test executable
+    //
+    // don't use the `cc` crate because we really want to use GCC and this
+    // specific set of flags
+    Command::new("gcc")
+        .arg("-Os")
+        .arg("-gdwarf")
+        .arg("-o")
+        .arg("test-data/x86_64_callconv")
+        .arg("test-data/x86_64_callconv.c")
+        .status()
+        .unwrap();
+}
